9ec738a8a216b9ab85ac7a7e2817514d360a7662
Suman Islam <sumanislam1416@gmail.com>
2022-06-02 15:22:46 +0600

    add descriptions for types and fileds of Track and Author

--- server/src/schema.js
+++ server/src/schema.js
@@ -4,16 +4,24 @@ const typeDefs = gql`
   "A Track is a group of module that teaches about a specific topic"
   type Track {
     id: ID!
+    "Tracks Title"
     title: String!
+    "Tracks Main Author"
     author: Author!
+    "Track's main illustration to display in track card or track page details"
     thumbnail: String
+    "Tracks approximate time to complete, in minutes"
     length: Int
+    "Number of modules Track contains"
     moduleCount: Int
   }
 
+  "Author of a complete Track"
   type Author {
     id: ID!
+    "Name of the Author"
     name: String!
+    "URL that contains photo of the Author"
     photo: String
   }
 `;
